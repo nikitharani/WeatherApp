bded3b481727f6bcdd6760b34820b80b59d32db6
Nikitha <nikithanalumachu@gmail.com>
2020-09-22 14:51:53 +0100

    WeatherApp first commit

---
+++ libs/php/getCityNames.php
@@ -0,0 +1,41 @@
+<?php
+// $file=file('worldcities.csv');
+
+$csv = array_map("str_getcsv", file("worldcities.csv")); 
+$header = array_shift($csv); 
+// Seperate the header from data
+
+$col = array_search("city", $header); 
+ foreach ($csv as $row) {      
+ $a[] = $row[$col]; 
+}
+
+
+// get the q parameter from URL
+if (!empty($_REQUEST['q']))
+{
+$q = $_REQUEST["q"];
+
+$hint = "";
+
+// lookup all hints from array if $q is different from ""
+if ($q !== "") {
+  $q = strtolower($q);
+  $len=strlen($q);
+  foreach($a as $name) {
+    if (stristr($q, substr($name, 0, $len))) {
+      if ($hint === "") {
+        $hint = $name;
+      } else {
+        $hint .= ", $name";
+      }
+    }
+  }
+}
+
+// Output "no suggestion" if no hint was found or output correct values
+echo $hint === "" ? "no suggestion" : $hint;
+}
+
+
+?>
